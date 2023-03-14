048520fdb8f7d6c320be893c6554bd83ad2a6335
SeokSeokNa <nsk7640@naver.com>
2023-03-14 22:45:46 +0900

    traceId 동기화 방식으로 변경 및 동시성 문제 발생

--- src/main/java/hello/advanced/trace/logtrace/FieldLogTrace.java
+++ src/main/java/hello/advanced/trace/logtrace/FieldLogTrace.java
@@ -1,46 +1,46 @@
-package hello.advanced.trace.hellotrace;
+package hello.advanced.trace.logtrace;
 
 import hello.advanced.trace.TraceId;
 import hello.advanced.trace.TraceStatus;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 
 @Slf4j
-@Component
-public class HelloTraceV2 {
+public class FieldLogTrace implements LogTrace{
     private static final String START_PREFIX = "-->";
     private static final String COMPLETE_PREFIX = "<--";
     private static final String EX_PREFIX = "<X-";
 
-    //트랜잭션 시작시 로그 시작
+    private TraceId traceIdHolder; //로그를 남겨야하는 모든 메소드에 TraceId 파라미터를 넘기지 않기위함(클래스 내부에 관리되는 필드로 메소드마다 꺼내쓰기) , 동시성 이슈가 발생..
+
+    @Override
     public TraceStatus begin(String message) {
-        TraceId traceId = new TraceId();
+        syncTraceId();
+        TraceId traceId = this.traceIdHolder;
         Long startTimeMs = System.currentTimeMillis();
         log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX,
                 traceId.getLevel()), message);
         return new TraceStatus(traceId, startTimeMs, message);
     }
 
-    //V2에서 추가(다음 depth 표현을 하기위해 .. controller -> service -> repository..)
-    public TraceStatus beginSync(TraceId beforeTraceId, String message) {
-//        TraceId traceId = new TraceId();
-        TraceId nextId = beforeTraceId.createNextId();
-        Long startTimeMs = System.currentTimeMillis();
-        log.info("[{}] {}{}", nextId.getId(), addSpace(START_PREFIX, nextId.getLevel()), message);
-        return new TraceStatus(nextId, startTimeMs, message);
+    //TraceId 동기화
+    private void syncTraceId() {
+        if (traceIdHolder == null) { //트랜젝션 시작시 처음 로그를 위해
+            traceIdHolder = new TraceId();
+        } else {
+            traceIdHolder = traceIdHolder.createNextId(); //트랜잭션 진행중 다음 로그를 위해
+        }
     }
 
-    //로그 끝
+    @Override
     public void end(TraceStatus status) {
         complete(status, null);
     }
 
-    //에러 발생시
+    @Override
     public void exception(TraceStatus status, Exception e) {
         complete(status, e);
     }
 
-    //완료 여부(정상 , 오류)
     private void complete(TraceStatus status, Exception e) {
         Long stopTimeMs = System.currentTimeMillis();
         long resultTimeMs = stopTimeMs - status.getStartTimeMs();
@@ -53,7 +53,19 @@ public class HelloTraceV2 {
                     addSpace(EX_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs,
                     e.toString());
         }
+
+        releaseTraceId();
     }
+
+    //로그 표기가 모두 완료되고 해당 TraceId 파괴하기
+    private void releaseTraceId() {
+        if (traceIdHolder.isFirstLevel()) { //로그의 시작도 depth가 1 이지만 끝도 1이라서
+            traceIdHolder = null;//파괴
+        } else {
+            traceIdHolder = traceIdHolder.createPreviousId(); //depth 하나씩 줄여가기
+        }
+    }
+
 
     //로그 표현
     private static String addSpace(String prefix, int level) {
